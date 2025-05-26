24831b425a1150f21116f6708e12340456b614cd
Jyosmitha <mjyosmitha@gmail.com>
2025-05-26 11:48:22 +0530

    updated the model name

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def is_english(text):
 def init_graph(api_key: str):
     
     global llm
-    llm  = ChatGroq(model="qwen-qwq-32b", api_key=api_key)
+    llm  = ChatGroq(model="deepseek-r1-distill-llama-70b", api_key=api_key)
     
     builder = StateGraph(BlogState)
     
